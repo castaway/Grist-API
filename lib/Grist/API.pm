02719a9cf3598e96f1b3a3316999751ce7c707ad
James Mastros <james@mastros.biz>
2025-05-09 22:31:53 +0100

    Docs + Test

--- lib/Grist/API.pm
+++ lib/Grist/API.pm
@@ -1,5 +1,27 @@
 package Grist::API;
 
+=head1 NAME
+
+Grist::API
+
+=head1 SYNOPSIS
+
+    my $g_api = Grist::API->new(
+      site => 'mygristsite',
+      access_key => 'myaccesskey'
+    );
+
+    # all workspaces and their documents
+    my $workspaces = $g_api->get_workspaces();
+
+    # the workspace I wanted:
+    my ($test_workspace) = grep {$_->{name} eq 'My Workspace'} @$workspaces;
+
+    # the document I wanted:
+    my ($doc) = grep { $_->{name} eq 'My Doc' } @{$test_workspace->{docs}};
+
+=cut
+
 use strictures 2;
 use v5.30;
 use REST::Client;
@@ -55,11 +77,22 @@ has rc_client => (is => 'lazy',
                        $client->setFollow(1);
                        push @{$client->getUseragent->requests_redirectable}, 'POST';
                        return $client;
-                       
                   });
 has workspaces => (is => 'rw');
+has documents  => (is => 'rw');
 has tables     => (is => 'rw');
-has apps       => (is => 'rw');
+
+=head3 get_workspaces
+
+Fetches and returns an arrayref of all the workspaces for this site. Each workspace is a hashref. The "name" key contains the workspace name, the "docs" key contains an arrayref of doc hashrefs, each with its own "name" key.
+
+The results of the latest call to B<get_workspaces> is saved in the attribute L<workspaces>.
+
+Calls L<https://support.getgrist.com/api/#tag/workspaces>
+
+    my $workspaces = $g_api->get_workspaces();
+
+=cut
 
 sub get_workspaces {
     my ($self) = @_;
@@ -71,6 +104,18 @@ sub get_workspaces {
 
     return $workspaces;
 }
+
+=head3 get_tables
+
+Fetches and returns an arrayref of all the tables for a given document. Pass in the document hashref (as returned by L<get_workspaces> for each workspace.
+
+The results of the latest call to B<get_tables> is saved in the attribute L<tables>.
+
+Calls L<https://support.getgrist.com/api/#tag/tables>
+
+    my $tables = $g_api->get_tables(doc => $mydocument);
+
+=cut
 
 sub get_tables {
     my ($self, %args) = @_;
@@ -92,9 +137,11 @@ sub get_tables {
 
 Returns an array-ref of records, each of which is a hash-ref keyed by column and with values of ... well, the values.
 
-Most values will be their obvious forms.  Booleans will come out as whatever the JSON module gives -- a JSON::PP::Boolean object under old perls, or the one true true/false value under newer perls.  (FIXME: verify and document what version of perl this changes in.)
+Most values will be their obvious forms.  Toggles will come out as whatever the JSON module gives -- a JSON::PP::Boolean object under old perls, or the one true true/false value under newer perls.  (FIXME: verify and document what version of perl this changes in.)  Dates are given as a unix time at gmt midnight at the beginning of the day.  An unfilled reference will be `0`, a filled reference will be the id of the record linked to.  A reference list will be an array-ref `['L', ...]`, where `...` are the row numbers.  Attachments are `['L', 1]` - I haven't worked out what this means yet.
 
-The `doc` and `table` named arguments are handled by this module, and should be the doc and table as returned by `->get_workspaces` and `->get_tables`.  (Or, at least, hashrefs with id fields.)  All other arguments are passed as query parameters, and you can use this mechinisim to pass `filter`, `sort`, `limit`, and `hidden`.  These should be plain strings as documented at https://support.getgrist.com/api/#tag/records/operation/listRecords.  What happens when you pass references is subject to change in the future.
+There is also one pseudo-column added by this method, `" id"` (yes, with a space -- it cannot clash with any normal column name this way).  It is an integer.
+
+The `doc` and `table` named arguments are handled by this module, and should be the doc and table as returned by `->get_workspaces` and `->get_tables`.  (Or, at least, hashrefs with id fields.)  All other arguments are passed as query parameters, and you can use this mechinisim to pass `filter`, `sort`, `limit`, and `hidden`.  These should be plain strings as documented at L<https://support.getgrist.com/api/#tag/records/operation/listRecords>.  What happens when you pass references is subject to change in the future.
 
 If you pass `hidden => "true"` as an argument, the module will not attempt to split the fields between user-supplied and internal fields.  The only hidden field I know of at present is `manualSort`.
 
@@ -109,21 +156,77 @@ sub get_records {
     $uri->query_form(%args);
 
     my $json = decode_json($self->rc_client->GET($uri)->responseContent());
-    print Dumper($json) if $self->debug;
+    # print Dumper($json) if $self->debug;
 
-    return $json->{records};
+    my $ret = [map { +{ %{$_->{fields}}, ' id' => $_->{id} }  } @{$json->{records}} ];
+    print Dumper($ret) if $self->debug;
+
+    return $ret;
 }
+
+=head3 update_record
+
+Given a doc, a table and a record, update the record.
+
+    ->update_record(doc => DOC_ID, table => TABLE_ID, record => RECORD_ID, fields => ... );
+
+=cut
+
+sub update_record {
+    my ($self, %args) = @_;
+
+    my $doc_id = (delete $args{doc})->{id};
+    my $table_id = (delete $args{table})->{id};
+
+    my $record = delete $args{record};
+    # a Grist record is { fields => {}, id => $val }
+    my $record_id = delete $record->{' id'};
+    my $g_record = { fields => $record, id => $record_id };
+
+    my $uri = URI->new("docs/$doc_id/tables/$table_id/records");
+    my $res = $self->rc_client->PATCH(
+        $uri,
+        encode_json({ records => [ $g_record ] })
+    );
+    return $res->responseCode() >= 200 && $res->responseCode() <= 300
+        ? 1: 0;
+}
+
+=head3 add_record
+
+=cut
+
+sub add_record {
+    my ($self, %args) = @_;
+
+    my $doc_id = (delete $args{doc})->{id};
+    my $table_id = (delete $args{table})->{id};
+
+    my $record = delete $args{record};
+    # a Grist record is { fields => {}, id => $val }
+    my $g_record = { fields => $record };
+
+    my $uri = URI->new("docs/$doc_id/tables/$table_id/records");
+    my $res = $self->rc_client->POST(
+        $uri,
+        encode_json({ records => [ $g_record ] })
+    );
+    return $res->responseCode() >= 200 && $res->responseCode() <= 300
+        ? 1: 0;
+}
+
+=head3 name_to_id
+
+Helper method, names are replaced by underscores.
+
+=cut
+
+sub name_to_id {
+    my ($self, $name) = @_;
+    $name =~ s/\s+/_/g;
+    return $name;
+}
+
 
 1;
 
-# print Dumper($app_data);
-# 'fields' => {
-#               'app_bundle_id' => 'com.foolsdog.soprafino',
-#               'Catalog_Description' => '',
-#               'Catalog_Copyright' => '',
-#               'IAP_Description' => undef,
-#               'Catalog_Credits' => '',
-#               'Full_App_Name' => 'Soprafino Tarot',
-#               'Abbreviated_App_Name' => '',
-#               'IAP_Price' => undef
-#             }
--- t/01basic.t
+++ t/01basic.t
@@ -6,6 +6,8 @@ use Test::More;
 use Config::General;
 use Data::Printer;
 
+use_ok('Grist::API');
+
 # keys.conf, or env vars:
 my $keys;
 if (-e 'keys.conf') {
@@ -23,10 +25,6 @@ if (-e 'keys.conf') {
     die "No keys found, create keys.conf or GRIST_KEY, GRIST_SITE env vars";
 }
 
-p $keys;
-
-use_ok('Grist::API');
-
 my $g_api = Grist::API->new(
     site       => $keys->{Keys}{Grist}{site},
     access_key => $keys->{Keys}{Grist}{key},
@@ -38,10 +36,11 @@ ok($g_api, 'Loaded grist API');
 # Test data names:
 my $ws_name = 'API Test';
 my $doc_name = 'First Document';
-my $table_name = 'First_Table';
+# API doesnt return us the table name anywhere!?
+my $table_id = $g_api->name_to_id('First Table');
+my $col1_name = $g_api->name_to_id('Col 1');
 
 my $workspaces = $g_api->get_workspaces();
-p $workspaces;
 
 is(ref $workspaces, 'ARRAY', '->get_workspaces returns arrayref');
 
@@ -55,11 +54,37 @@ SKIP: {
     my $tables = $g_api->get_tables(doc => $test_doc);
     ok(scalar @$tables > 0, 'Found some tables');
 
-    my ($test_table) = grep {$_->{id} =~ /$table_name/ } @$tables;
+    my ($test_table) = grep {$_->{id} =~ /$table_id/ } @$tables;
     ok($test_table, 'Found test table');
 
     my $records = $g_api->get_records(doc => $test_doc, table => $test_table);
     ok(scalar @$records > 0, 'Found table records');
+
+    my ($hello_world) = grep { $_->{$col1_name} eq 'Hello World' } @$records;
+    ok($hello_world, 'Found test row');
+
+    # Increase the numbers col value:
+    my $col2 = $g_api->name_to_id('Col 2');
+    $hello_world->{$col2}++;
+    my $status = $g_api->update_record(doc => $test_doc, table => $test_table, record => $hello_world);
+    ok($status, 'Updated numeric column');
+    # refetch record to compare:
+    my $new_records = $g_api->get_records(doc => $test_doc, table => $test_table);
+    my ($new_hw) = grep { $_->{$col1_name} eq 'Hello World' } @$new_records;
+    is($new_hw->{$col2},$hello_world->{$col2}, 'Increased col2 value');
+
+    # Add another record:
+    my $new_status = $g_api->add_record(
+        doc => $test_doc,
+        table => $test_table,
+        record => {
+            'Col_1' => 'New record',
+                'Col_2' => 10,
+                'Col_3' => 'true',
+                'Col_4' => '', # ???
+                'Col_5' => '2024-12-02',
+        });
+    ok($new_status, 'Added new record');
 };
 
 done_testing;
